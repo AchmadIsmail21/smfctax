acd107ddb110a9a30e489872720ef31decb92c48
achmad ismail <mac@macs-MacBook-Pro.local>
2025-01-21 17:48:59 +0700

    perbaikan layout

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,17 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
+// Menambahkan subset yang diperlukan
 const outfit = Outfit({
-  variable: "--font-geist-sans",weight:["400", "500", "600", "700"]
+  variable: "--font-geist-sans",
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin", "latin-ext"], // Menambahkan subset
 });
 
 const ovo = Ovo({
-  variable: "--font-geist-mono", weight:["400"]
+  variable: "--font-geist-mono",
+  weight: ["400"],
+  subsets: ["latin"], // Menambahkan subset
 });
 
 export const metadata = {
