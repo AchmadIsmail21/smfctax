15622f23dc3e2a5e375a041b1c3f45169ac653ea
achmad ismail <mac@macs-MacBook-Pro.local>
2025-01-21 17:23:11 +0700

    first commit

---
+++ app/components/Work.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Work = () => {
+    return (
+        <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+            <h4 className='text-center mb-2 text-lg font-Ovo'>My portofolio</h4>
+            <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
+            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. At minima et velit quae omnis! Rerum doloribus eaque nostrum culpa dolores, deleniti in perspiciatis minima molestias officiis amet? Harum, corrupti ea.
+            </p>
+        </div>
+    )
+}
+
+export default Work
